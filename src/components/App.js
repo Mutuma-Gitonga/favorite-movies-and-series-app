62932f4a1b18c6767d77175321980a3015d15263
Mutuma Gitonga <mgitonga150@gmail.com>
2022-10-06 10:49:19 +0300

    add NavBar and Home components

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {Switch, Route} from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
