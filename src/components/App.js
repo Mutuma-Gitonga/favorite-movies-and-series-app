6b50a51650ac666a880b98d6ae8498c2ea9aa2d9
Mutuma Gitonga <mgitonga150@gmail.com>
2022-10-07 18:48:32 +0300

    add a callback function to App component to POST new movie object

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Switch, Route} from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
@@ -7,13 +7,24 @@ import AddAFavoriteMovieForm from "./AddAFavoriteMovieForm";
 
 
 function App() {
+
+  function handleAddNewMovie(newMovieObj) {
+    fetch("http://localhost:3000/movies", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newMovieObj)
+  })
+  }
+  
   return (
     <div className="App">
       <NavBar />
       <Switch>
         <Route exact path="/"><Home /></Route>
         <Route path="/movies"><MoviesList /></Route>
-        <Route path="/newFavoriteMovie"><AddAFavoriteMovieForm /></Route>
+        <Route path="/newMovie"><AddAFavoriteMovieForm onSubmit={handleAddNewMovie} /></Route>
       </Switch>
     </div>
   );
