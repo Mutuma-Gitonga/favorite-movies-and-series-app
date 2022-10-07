2cb4aa3728711d6292f2da5a283a6212af6dcaaf
Mutuma Gitonga <mgitonga150@gmail.com>
2022-10-08 00:59:27 +0300

    edit Home Page to guide user

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Welcome to your Favorite Movies App</h1>
+      <h1 style={{textAlign:"center", color:"blue"}}>Welcome to your Favorite Movies App<br/>Click on "Movies" link to start viewing listings, liking, or deleting them. You can also, add your favorite movie by clicking on the "Add a Favorite Movie" above.</h1>
     </div>
   );
 }
