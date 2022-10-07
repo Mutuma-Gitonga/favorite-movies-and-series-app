f49f2e6faf9315e337b156f7b8a1d2358ec9d1c9
Mutuma Gitonga <mgitonga150@gmail.com>
2022-10-07 13:03:33 +0300

    add PATCH method to add and remove favorite movies (toggle between true & false)

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,40 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 
-function Movie({fetchedMovie}) {
+function Movie({fetchedMovie, onMovieFavorite, favoriteButtonState}) {
+  // const [removeFavorite, setRemoveFavorite] = useState(false);
+  // const [favoriteState, setFavoriteState] = useState();
+  const favoriteRef = useRef(true);
+
+  console.log(favoriteButtonState)
+  function handleClick() {
+    console.log("favoriteButtonState in favorite",favoriteButtonState)
+    if(favoriteButtonState) {
+      // setFavoriteState(false);
+      // console.log("favorite state under favorites",favoriteState);
+      favoriteRef.current = false;
+    } else {
+      // console.log("favorite state",favoriteState);
+      // setFavoriteState(true);
+      favoriteRef.current = true;
+    }
+    console.log("favoriteRef", favoriteRef.current)
+
+    // Persist true favorite boolean status in the db.json file when "add to favorites button's clicked"
+    fetch(`http://localhost:3000/movies/${fetchedMovie.id}`, {
+      method: "PATCH",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({favorite: favoriteRef.current})
+      })
+      .then(res => res.json())
+        // Update the item favorite status in state using callback (inverse data flow)
+        // Ensures the newly favorited items appear on the frontend
+        .then(movie => onMovieFavorite(movie.id, favoriteRef.current))
+
+    // console.log("Removed movie ID",fetchedMovie.id);
+  }
+ 
   return (
     <div>
       <ul style={{listStyleType: "none"}}>
@@ -10,7 +44,7 @@ function Movie({fetchedMovie}) {
           <p style={{textAlign:"center", color: "blue"}}>{fetchedMovie.Title}</p>
           
           {/* Adding to favorites === PATCH operation */}
-          <button style={{margin: "auto", display: "block"}}>Add to Favorites &hearts;</button>
+          <button style={{margin: "auto", display: "block"}} onClick={handleClick}>{favoriteButtonState ? "Remove from Favorites": "Add to Favorites &hearts;"}</button>
           <hr/>
         </li>
       </ul> 
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -5,13 +5,18 @@ function MoviesList() {
   // State variable to store fetched movies
   const [fetchedMovies, setFetchedMovies] = useState([]);
   const [favoriteButtonState, setFavoriteButtonState] = useState(false);
+  const [areFavoriteMoviesAvailable, setAreFavoriteMoviesAvailable] = useState(true);
 
   // Fetch movies list when user clicks on Movies
   useEffect(() => {
     fetch('http://localhost:3000/movies')
       .then(res => res.json())
         .then(moviesDb => setFetchedMovies(moviesDb))
+
+    // setFavoriteButtonState((favoriteButtonState) => favoriteButtonState = false);
   },[]);
+
+  console.log(fetchedMovies);
 
   function handleClick() {
     setFavoriteButtonState((favoriteButtonState) => !favoriteButtonState);
@@ -23,7 +28,9 @@ function MoviesList() {
       const filteredMovies = fetchedMovies.filter((fetchedMovie) => {
         return fetchedMovie.favorite === true;
       });
-      console.log(filteredMovies);
+      
+      filteredMovies.length !== 0 ? setAreFavoriteMoviesAvailable(areFavoriteMoviesAvailable => areFavoriteMoviesAvailable = true) : setAreFavoriteMoviesAvailable(areFavoriteMoviesAvailable => areFavoriteMoviesAvailable = false);
+
       setFetchedMovies(() => filteredMovies);
     } else {
       fetch('http://localhost:3000/movies')
@@ -32,13 +39,29 @@ function MoviesList() {
     }
   },[favoriteButtonState]);
 
+  // Callback function to update fetchedMovies frontend state following PATCH operation
+  function handleMovieFavorite(patchedMovieId, favState) {
+    console.log(patchedMovieId,favState);
+    
+    const updatedMoviesAfterPatch = fetchedMovies.map((movie) => {
+      if(movie.id === patchedMovieId){
+        return {...movie, favorite: favState};
+      } else {
+        return movie;
+      }
+    });
+    setFetchedMovies(() => updatedMoviesAfterPatch);
+  }
+
   return (
     <div>
       <button id="favoriteListButton" style={{margin: "auto", display: "block"}} onClick={handleClick}>{favoriteButtonState? "View All Movies" : "View Favorite Movies"}</button>
-      {
-        fetchedMovies.map(fetchedMovie => {
-          return <Movie key={fetchedMovie.id} fetchedMovie={fetchedMovie} />
-        })
+      
+      { areFavoriteMoviesAvailable || !favoriteButtonState ?
+          fetchedMovies.map(fetchedMovie => {
+            return <Movie key={fetchedMovie.id} fetchedMovie={fetchedMovie} onMovieFavorite={handleMovieFavorite} favoriteButtonState={favoriteButtonState} />
+          }) :
+            <p style={{textAlign: "center", color: "red", fontSize: "2em" }}>No favorite movies at the moment. Please click on "View all Movies" to add your favorite ones.</p>
       }
     </div>
   )
